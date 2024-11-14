a884ef4e8784df35a9fbad17563b118bdd37a21f
Emmanuel Nifakos <marcopolofl@gmail.com>
2024-11-13 19:24:35 -0500

    Redirect user from dashboard to login if not signed in

--- client/src/routes/dashboard/Dashboard.jsx
+++ client/src/routes/dashboard/Dashboard.jsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
+import {useUser} from '@clerk/clerk-react';
+import {useNavigate} from 'react-router-dom';
 
 function Dashboard() {
+    const navigate = useNavigate();
+    const {isSignedIn} = useUser();
+
+    if (!isSignedIn) {
+        navigate('/login');
+    }
+
     return (
         <>
             <h2>Dashboard</h2>
