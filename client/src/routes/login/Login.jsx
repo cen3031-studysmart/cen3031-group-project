bcb9b22fac5a596f38e42cd2bddcb3d0b3592b7b
Emmanuel Nifakos <marcopolofl@gmail.com>
2024-11-13 19:25:09 -0500

    Add SignIn component to login page

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import {SignIn, useUser} from '@clerk/clerk-react';
+
+const port = import.meta.env.PORT | 5173;
 
 function Login() {
     return (
         <>
             <h2>Login</h2>
+            <SignIn forceRedirectUrl={`http://localhost:${port}/dashboard`} />
         </>
     );
 }
