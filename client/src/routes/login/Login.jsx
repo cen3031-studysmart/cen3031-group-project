9e4fbe25be9d95667e0017645f40fee005bd6fb8
Emmanuel Nifakos <marcopolofl@gmail.com>
2024-11-13 19:25:27 -0500

    Redirect user from login to dashboard if signed in

--- client/src/routes/login/Login.jsx
+++ client/src/routes/login/Login.jsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import {SignIn, useUser} from '@clerk/clerk-react';
+import {useNavigate} from 'react-router-dom';
 
 const port = import.meta.env.PORT | 5173;
 
 function Login() {
+    const navigate = useNavigate();
+    const {isSignedIn} = useUser();
+
+    if (isSignedIn) {
+        navigate('/dashboard');
+    }
+
     return (
         <>
             <h2>Login</h2>
